7fa4f07eeb5b58f6f6570c0d339536681ad1af98
UndeadScythes <daveyognaut2@gmail.com>
2012-12-23 12:11:50 +0000

    Tidied up code

--- src/periodstability/PeriodStability.java
+++ src/periodstability/PeriodStability.java
@@ -10,37 +10,32 @@ import prng.*;
  * @author UndeadScythes
  */
 public class PeriodStability {
-
     /**
      * @param args The command line arguments
      */
     public static void main(final String[] args) throws IOException {
         final BufferedWriter out = new BufferedWriter(new FileWriter("PeriodStability.csv"));
-        for(int n = 3; n <= 29; n++) {
-            final long order = (1 << n) - 1;
+
+        for(int n = 2; n <= 30; n++) {
             final Polynomial primitive = Polynomial.getPrimitive(n, 0);
-            final List<Long> factors = Factor.getPrimeFactors(order);
-            System.out.println("n = " + n + "\to = " + order + "\tf = " + primitive.getLong() + "\tqs = " + factors.toString());
-            for(long q : factors) {
+            final int order = (1 << n) - 1;
+            final List<Integer> factors = Factor.getPrimeFactors(order);
+            System.out.println("n = " + n + "\to = " + order + "\tf = " + primitive.toInt() + "\tqs = " + factors.toString());
+            for(int q : factors) {
                 if(q > 17 && q < order) {
+                    final int mid = q / 2;
+                    final int r = order / q;
+                    int[] weights = new int[r];
                     int i = 0;
-                    long sum = 0;
-                    final int r = (int)(order / q);
-                    final long mid = q / 2;
-                    long[] weights = new long[r];
+                    int sum = 0;
                     final GaloisLFSR lfsr = new GaloisLFSR(n, primitive, 1);
                     do {
-                        if(lfsr.getBit(0) == 1) {
-                            weights[i]++;
-                        }
+                        weights[i] += lfsr.getBit(0);
                         i = (i + 1) % r;
                         lfsr.clock();
                     } while(lfsr.getState() != 1);
                     for(i = 0; i < r; i++) {
-                        if(weights[i] > mid) {
-                            weights[i] = q - weights[i];
-                        }
-                        sum += weights[i];
+                        sum += (weights[i] > mid) ? q - weights[i] : weights[i];
                     }
                     System.out.println("\tq = " + q + "\tr = " + r + "\ts = " + sum);
                     out.write(n + "," + q + "," + Long.toString(sum));
